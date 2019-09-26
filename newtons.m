98eee9955f10061ef863e41286fe4839be7d8f23
Domonkos Skublics <skubidomi@gmail.com>
2019-09-26 13:57:53 +0200

    derivált függvényként átadva

--- newtons.m
+++ newtons.m
@@ -1,7 +1,7 @@
-function [x, fx, loops] = newtons (P, x)
+function [x, fx, loops] = newtons (P,dP, x)
 loops = 0;
 while  1E-8<abs(polyval(P, x)) % difference is smaller than 0.00000001
-    x = x-polyval(P, x)/deriv(P, x); % the method
+    x = x-polyval(P, x)/polyval(dP, x); % the method
     loops = loops + 1; % loop counter
 end
 fx = polyval(P, x);
--- test.m
+++ test.m
@@ -1,6 +1,7 @@
-% polinom
+% polynom
 P = [1 2 10 -20];
+dP = [3 4 10];
 % starting point
 x0 = 1;
 % newtons method
-[xk, fxk, k] = newtons(P, x0);
+[xk, fxk, k] = newtons(P ,dP ,x0);
