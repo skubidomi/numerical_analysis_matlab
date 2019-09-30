bc021a9a47e6bbed09b7e4e81215037a3f16cc1f
Mórász Bálint <balint@morasz.hu>
2019-09-30 21:55:24 +0200

    merge tests

--- lab1/test.m
+++ lab1/test.m
@@ -11,3 +11,12 @@ fprintf('value of x_k is %1.10e\n',xk)
 % secant method
 [xk2, fxk2, k2] = secant(P, 1, 2);
 fprintf('value of xk2 is %1.10e\n', xk2)
+
+% bisection method
+[x1, x2] = bisection(@f, 1, 2, 3);
+fprintf('value of x after 3 steps is %1.10e\n',x1)
+
+function y = f(x)
+    % y = x^3 + 2*x^2 + 10 * x - 20;
+    y = polyval(P, x);
+end
