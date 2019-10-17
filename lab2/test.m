9b7984d07acee1c1cd8660939c5f826153973e07
Balint Morasz <balint@morasz.hu>
2019-10-17 14:06:56 +0200

    complete lab 2.b.

--- lab2/test.m
+++ lab2/test.m
@@ -4,13 +4,34 @@ N = 13;
 
 y = midpoint(f, 0, pi/2, N);
 
-fprintf('[midpoint]: integrate of sin is %1.10e\n', y)
+fprintf('[midpoint]: integrate of sin with %i points is %1.10e\n', N, y)
 
 y = trapezoid(f, 0, pi / 2, N);
 
-fprintf('[trapezoid]: integrate of sin is %1.10e\n', y)
+fprintf('[trapezoid]: integrate of sin with %i points is %1.10e\n',N, y)
 
 y = simpsons(f, 0, pi / 2, N);
 
-fprintf('[simpsons]: integrate of sin is %1.10e\n', y)
+fprintf('[simpsons]: integrate of sin with %i points is %1.10e\n',N, y)
 
+y = simpsons(f, 0, pi / 2, 2*N);
+
+fprintf('[simpsons]: integrate of sin with %i points is %1.10e\n',2*N, y)
+
+r_mid = error_rate(@midpoint, 10);
+
+nexttile
+plot(r_mid);
+title('Midpoint')
+
+r_trap = error_rate(@trapezoid, 10);
+
+nexttile
+plot(r_trap);
+title('Trapezoid')
+
+r_sim = error_rate(@simpsons, 10);
+
+nexttile
+plot(r_sim);
+title('Simpsons')
