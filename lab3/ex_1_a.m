c8665288a1ce15fb746780b5eaf939ec2e40d7da
Mórász Bálint <balint@morasz.hu>
2019-11-22 01:08:50 +0100

    lab 3 / 1_a

---
+++ lab3/ex_1_a.m
@@ -0,0 +1,18 @@
+
+f = @(x) 1 / (1 + x^2);
+
+E = zeros(12,2);
+for i = 1:12
+    step = 10/(2*i);
+    X = -5:step:5;
+    h = lagrange(f, X);
+    test = zeros(1000);
+    for j = -5:0.001:5
+        idx = round((j + 5) * 10000 +1);
+        test(idx) = h(j) - f(j);
+    end
+    E(i,1) = i*2;
+    E(i,2) = norm(test, inf);
+end
+
+plot(E(:,1), E(:,2));
--- lab3/lagrange.m
+++ lab3/lagrange.m
@@ -1,18 +1,18 @@
-function g = lagrange(f, from, until, k)
+function g = lagrange(f, X)
 %LAGRANGE Summary of this function goes here
 %   Detailed explanation goes here
 
-step = (until - from) / (k-1); 
-X = from:step:until;
-g = @x 0;
-for i = 0:k
-    h = @x 1;
-    for j = 0:k
+N = length(X);
+
+g = @(x) 0;
+for i = 1:N
+    h = @(x) 1;
+    for j = 1:N
         if(i ~= j)
-           h = @x: h(x) * (x - X(j)) / (X(i) - X(j)); 
+           h = @(x) h(x) * (x - X(j)) / (X(i) - X(j)); 
         end
     end
-    g = @x: g(x) + h(x)*f(X(i));
+    g = @(x) g(x) + h(x)*f(X(i));
 end
 
 end
