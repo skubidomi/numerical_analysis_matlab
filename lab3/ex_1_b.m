7953ae163991debd70f7b4661d49c28836413fa1
balint <balint@morasz.hu>
2019-11-25 16:50:26 +0100

    lab 3 1/b corrected

--- lab3/ex_1_b.m
+++ lab3/ex_1_b.m
@@ -1,21 +1,19 @@
 
-f = @(x) 1 / (1 + x^2);
+f = @(x) 1 ./ (1 + x.^2);
 
 E = zeros(12,2);
 for i = 1:12
     n = 2*i+1;
-    X = zeros(n,1);
-    for k = 1:n
-        X(k) = 5*cos( ((2*(n-k)-1) / 2*n) * pi);
-    end
+    X = 5.*cos( (2*(n-1:-1:0)+1) / (2*n) * pi );
     h = lagrange(f, X);
-    test = zeros(1000);
-    for k = -5:0.001:5
-        idx = round((k + 5) * 1000 +1);
-        test(idx) = h(k) - f(k);
+    H = zeros(10000,1);
+    F = f(-5:0.001:5)';
+    for j = -5:0.001:5
+        idx = round((j + 5) * 1000 +1);
+        H(idx) = h(j);
     end
     E(i,1) = i*2;
-    E(i,2) = norm(test, inf);
+    E(i,2) = norm(H - F, inf);
 end
 
 plot(E(:,1), E(:,2));
